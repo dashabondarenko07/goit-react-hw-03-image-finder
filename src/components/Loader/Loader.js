bd061c0cae10ea42674d564466d888e11867cc53
dashabondarenko07 <dashabondarenko0709@gmail.com>
2022-08-08 19:07:10 +0300

    fix

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -1,5 +1,5 @@
 import { Audio } from 'react-loader-spinner';
 
 export const Loader = () => {
-  return <Audio height="100" width="center" color="#3f51b5" />;
+  return <Audio height="100" width='100%' color="#3f51b5" />;
 };
